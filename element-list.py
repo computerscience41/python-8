bd32689be95efd55bcf4729cdbb42f683e6c3c45
computerscience41 <rakibhasan934541@gmail.com>
2024-11-26 17:04:41 +0600

    element

---
+++ element-list.py
@@ -0,0 +1,5 @@
+# Combine list element
+
+fruits = ["apple", "banana","cherry"]
+for index, fruits in enumerate(fruits):
+    print(f"Index: {index},Fruits: {fruits}")
---
+++ range.py
@@ -0,0 +1,5 @@
+# The enumerate function()
+
+
+for i in range (1 , 11 , 2):
+    print(i)
